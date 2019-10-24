4529ad9b80075e1dcabc69fe3a2a96fd7994991a
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-10-24 12:52:40 +0200

    fix(condition): invert IfNot filter logic

--- src/decorator/condition/if-not.ts
+++ src/decorator/condition/if-not.ts
@@ -6,7 +6,7 @@ import {TFunction} from "@/types";
 export function IfNot(...fns: TFunction<boolean>[]) {
   return (target: any, key: string, desc: TypedPropertyDescriptor<TFunction>) => {
     wrapFunction(desc, function({args, orig}) {
-      if (fns.find(fn => !fn.apply(this, args))) return;
+      if (fns.find(fn => fn.apply(this, args))) return;
       return orig.apply(this, args);
     });
   };
