bce2b9c71dbbc77d3b940ed5a97c07e82e3ff6e5
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-10-26 19:53:34 +0200

    chore(docs): document vue ref conditions

--- src/conditions/events/vue/is-in-path.ts
+++ src/conditions/events/vue/is-in-path.ts
@@ -2,6 +2,29 @@ import {Vue} from "vue/types/vue";
 
 
 
+/**
+ * Creates a predicate that checks if its first parameter (assumed event) has
+ * one or more given vue refs in its {@link Event#composedPath | composedPath}.
+ *
+ * @param refs - The vue refs to check against
+ * @returns A predicate that returns true if all refs are part of the events composedPath.
+ *
+ * @remarks
+ * This function is meant to be a condition for {@link If} alike methods.
+ * If you want to check if one of many matches use {@link isOneInPath} instead.
+ *
+ * Example:
+ * ```TS
+ *   @If(isInPath("drawer"))
+ *   private doSomething(ev: Event) {
+ *     // this.$refs["drawer"](.$el) is a member of ev.composedPath()
+ *   }
+ * ```
+ *
+ * {@link If} {@link IfNot} {@link or} {@link and} {@link not}
+ * {@link isOneInPath}
+ * @public
+ */
 export function isInPath(...refs: string[]) {
   return function(this: Vue, ev: Event) {
     if (!ev || !ev.composedPath) return false;
--- src/conditions/events/vue/is-one-in-path.ts
+++ src/conditions/events/vue/is-one-in-path.ts
@@ -2,6 +2,29 @@ import {Vue} from "vue/types/vue";
 
 
 
+/**
+ * Creates a predicate that checks if its first parameter (assumed event) has
+ * one or more given vue refs in its {@link Event#composedPath | composedPath}.
+ *
+ * @param refs - The vue refs to check against
+ * @returns A predicate that returns true if one of all refs are part of the events composedPath.
+ *
+ * @remarks
+ * This function is meant to be a condition for {@link If} alike methods.
+ * If you want to check if all match use {@link isInPath} instead.
+ *
+ * Example:
+ * ```TS
+ *   @If(isOneInPath("drawer", "navbar"))
+ *   private doSomething(ev: Event) {
+ *     // this.$refs["drawer"](.$el) or this.$refs["navbar"](.$el) is a member of ev.composedPath()
+ *   }
+ * ```
+ *
+ * {@link If} {@link IfNot} {@link or} {@link and} {@link not}
+ * {@link isInPath}
+ * @public
+ */
 export function isOneInPath(...refs: string[]) {
   return function(this: Vue, ev: Event) {
     if (!ev || !ev.composedPath) return false;
