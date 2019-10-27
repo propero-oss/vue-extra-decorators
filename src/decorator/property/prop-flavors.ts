32dd25001ce3da8ba45ccd6dfb990dd54f2f7f7c
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-10-27 03:06:05 +0100

    chore(docs): document property decorators

--- src/decorator/property/prop-flavors.ts
+++ src/decorator/property/prop-flavors.ts
@@ -2,18 +2,151 @@ import {Prop, PropOpts} from "@/decorator";
 import {TypedVueDecorator} from "@/types";
 
 
+
+/**
+ * Declares a Vue property.
+ * String flavor of {@link Prop | @Prop} decorator
+ * @param opts - The property options to pass to vue, it can either be
+ *  a constructor function like String or Number, an array of constructor
+ *  functions or a property object {@link RecordPropOpts}.
+ *  If a property object is given, in addition to the normal vue options
+ *  some additional values can be provided.
+ *
+ * @remarks
+ * There are various preconfigured flavors of this decorator for different
+ * types already available:
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp}
+ *
+ * Additional Options:
+ * `opts.default`   | {@link RecordPropOpts#default}
+ * `opts.literal`   | {@link RecordPropOpts#literal}
+ * `opts.required`  | {@link RecordPropOpts#required}
+ * `opts.twoWay`    | {@link RecordPropOpts#twoWay}
+ * `opts.validator` | {@link RecordPropOpts#validator}
+ * `opts.model`     | {@link RecordPropOpts#model}
+ *
+ * @example
+ * ```TS
+ *   @SProp()                      private simpleString!: string;
+ *   @SProp({default: null})       private defaultsToNull!: null | string;
+ *   @SProp({default: () => "hi"}) private defaultsToHi!: string;
+ *   @SProp({model: "change"})     private modelValue!: string;
+ * ```
+ *
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp} {@link Prop}
+ * @public
+ */
 export function SProp(opts?: PropOpts<string>): TypedVueDecorator<string> {
   return Prop<string>(opts, String as any);
 }
 
+/**
+ * Declares a Vue property.
+ * Number flavor of {@link Prop | @Prop} decorator
+ * @param opts - The property options to pass to vue, it can either be
+ *  a constructor function like String or Number, an array of constructor
+ *  functions or a property object {@link RecordPropOpts}.
+ *  If a property object is given, in addition to the normal vue options
+ *  some additional values can be provided.
+ *
+ * @remarks
+ * There are various preconfigured flavors of this decorator for different
+ * types already available:
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp}
+ *
+ * Additional Options:
+ * `opts.default`   | {@link RecordPropOpts#default}
+ * `opts.literal`   | {@link RecordPropOpts#literal}
+ * `opts.required`  | {@link RecordPropOpts#required}
+ * `opts.twoWay`    | {@link RecordPropOpts#twoWay}
+ * `opts.validator` | {@link RecordPropOpts#validator}
+ * `opts.model`     | {@link RecordPropOpts#model}
+ *
+ * @example
+ * ```TS
+ *   @NProp()                       private simpleNumber!: number;
+ *   @NProp({default: null})        private defaultsToNull!: null | number;
+ *   @NProp({default: Math.random}) private defaultsToRandom!: number;
+ *   @NProp({model: "change"})      private modelValue!: number;
+ * ```
+ *
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp} {@link Prop}
+ * @public
+ */
 export function NProp(opts?: PropOpts<number>): TypedVueDecorator<number> {
   return Prop<number>(opts, Number as any);
 }
 
+/**
+ * Declares a Vue property.
+ * Boolean flavor of {@link Prop | @Prop} decorator
+ * @param opts - The property options to pass to vue, it can either be
+ *  a constructor function like String or Number, an array of constructor
+ *  functions or a property object {@link RecordPropOpts}.
+ *  If a property object is given, in addition to the normal vue options
+ *  some additional values can be provided.
+ *
+ * @remarks
+ * There are various preconfigured flavors of this decorator for different
+ * types already available:
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp}
+ *
+ * Additional Options:
+ * `opts.default`   | {@link RecordPropOpts#default}
+ * `opts.literal`   | {@link RecordPropOpts#literal}
+ * `opts.required`  | {@link RecordPropOpts#required}
+ * `opts.twoWay`    | {@link RecordPropOpts#twoWay}
+ * `opts.validator` | {@link RecordPropOpts#validator}
+ * `opts.model`     | {@link RecordPropOpts#model}
+ *
+ * @example
+ * ```TS
+ *   @BProp()                  private simpleFlag!: boolean;
+ *   @BProp({default: null})   private defaultsToNull!: null | boolean;
+ *   @BProp({default: false})  private defaultsToFalse!: boolean;
+ *   @BProp({model: "change"}) private modelValue!: boolean;
+ * ```
+ *
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp} {@link Prop}
+ * @public
+ */
 export function BProp(opts?: PropOpts<boolean>): TypedVueDecorator<boolean> {
   return Prop<boolean>(opts, Boolean as any);
 }
 
+/**
+ * Declares a Vue property.
+ * Date flavor of {@link Prop | @Prop} decorator
+ * @param opts - The property options to pass to vue, it can either be
+ *  a constructor function like String or Number, an array of constructor
+ *  functions or a property object {@link RecordPropOpts}.
+ *  If a property object is given, in addition to the normal vue options
+ *  some additional values can be provided.
+ *
+ * @remarks
+ * There are various preconfigured flavors of this decorator for different
+ * types already available:
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp}
+ *
+ * Additional Options:
+ * `opts.default`   | {@link RecordPropOpts#default}
+ * `opts.literal`   | {@link RecordPropOpts#literal}
+ * `opts.required`  | {@link RecordPropOpts#required}
+ * `opts.twoWay`    | {@link RecordPropOpts#twoWay}
+ * `opts.validator` | {@link RecordPropOpts#validator}
+ * `opts.model`     | {@link RecordPropOpts#model}
+ *
+ * @example
+ * ```TS
+ *   @DProp()                            private simpleDate!: Date;
+ *   @DProp({default: null})             private defaultsToNull!: null | Date;
+ *   @DProp({default: () => new Date()}) private defaultsToNow!: Date;
+ *   @DProp({model: "change"})           private modelValue!: Date;
+ * ```
+ *
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp} {@link Prop}
+ * @public
+ */
 export function DProp(opts?: PropOpts<Date>): TypedVueDecorator<Date> {
   return Prop<Date>(opts, Date);
 }
--- src/decorator/property/prop.ts
+++ src/decorator/property/prop.ts
@@ -1,22 +1,105 @@
 import {Constructor, SingleArgFunction, TFunction, TypedVueDecorator} from "@/types";
 import {optionsExtension} from "@/vue";
 
+
+
+
+/**
+ * Parameters of the {@link Prop | @Prop} decorator and its flavors
+ * @public
+ */
 export type RecordPropOpts<T> = {
+  /**
+   * The type of the property. Should be a constructor function or
+   * an array of constructor functions.
+   */
   type?: Constructor<T> | Constructor<any>[];
+  /**
+   * A default value or a function constructing a default value.
+   * Arrays or objects are supported, if you want to use a function
+   * as a value, use {@link RecordPropOpts#literal} instead.
+   */
   default?: T | TFunction<T>;
+  /**
+   * A default value. If a function is given, it will be passed as a
+   * literal value instead of executed.
+   */
   literal?: T;
+  /**
+   * Whether or not the property is required.
+   */
   required?: boolean;
+  /**
+   * Whether or not the options is two way.
+   */
   twoWay?: boolean;
+  /**
+   * A validator function returning true if a given value is valid
+   * and false otherwise.
+   */
   validator?: SingleArgFunction<boolean, T>;
+  /**
+   * Marks this property as a model property, if true is given, it
+   * will use the event 'update:PROPNAME' as the binding trigger,
+   * where PROPNAME is the name of the decorated property. If a
+   * string is given, it will be used in place of the event instead.
+   */
   model?: boolean | string
 };
 
+/**
+ * Parameters of the {@link Prop | @Prop} decorator and its flavors
+ * @public
+ */
 export type PropOpts<T> = Constructor<T> | Constructor<any>[] | RecordPropOpts<T>;
 
+/**
+ * Checks if a given object is a type literal option or a record
+ * @param opts - the options to check
+ * @returns true, if the options is either an array or a constructor
+ * @internal
+ */
 export function isTypeLiteralOption<T>(opts: PropOpts<T>): opts is (Constructor<T> | Constructor<any>[]) {
   return typeof opts === "function" || Array.isArray(opts);
 }
 
+/**
+ * Declares a Vue property.
+ * @param opts - The property options to pass to vue, it can either be
+ *  a constructor function like String or Number, an array of constructor
+ *  functions or a property object {@link RecordPropOpts}.
+ *  If a property object is given, in addition to the normal vue options
+ *  some additional values can be provided.
+ * @param typeOverride - an optional type override if no type is specified
+ *  using both provided options or reflected decorator metadata.
+ *
+ * @remarks
+ * There are various preconfigured flavors of this decorator for different
+ * types already available:
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp}
+ *
+ * Additional Options:
+ * `opts.type`      | {@link RecordPropOpts#"type"}
+ * `opts.default`   | {@link RecordPropOpts#default}
+ * `opts.literal`   | {@link RecordPropOpts#literal}
+ * `opts.required`  | {@link RecordPropOpts#required}
+ * `opts.twoWay`    | {@link RecordPropOpts#twoWay}
+ * `opts.validator` | {@link RecordPropOpts#validator}
+ * `opts.model`     | {@link RecordPropOpts#model}
+ *
+ * @example
+ * ```TS
+ *   @Prop()                    private simpleFlag!: boolean;
+ *   @Prop(Number)              private simpleNumber!: number;
+ *   @Prop([String, Number])    private stringOrNumber!: string | number;
+ *   @Prop({default: null})     private defaultsToNull!: null | string;
+ *   @Prop({literal: parseInt}) private parser!: (str: string) => number;
+ *   @Prop({model: "change"})   private modelValue!: string;
+ * ```
+ *
+ * {@link SProp} {@link NProp} {@link DProp} {@link BProp}
+ * @public
+ */
 export function Prop<T>(opts: PropOpts<T> = {}, typeOverride?: Constructor<T>): TypedVueDecorator<T> {
 
   // Type literal to record def
