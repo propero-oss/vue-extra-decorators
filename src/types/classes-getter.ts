6fdd7c485b3ce318c77693765223b3cfcfbacd52
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-12-16 23:31:09 +0100

    fix: adjust typings for string values

--- src/types/classes-getter.ts
+++ src/types/classes-getter.ts
@@ -5,4 +5,4 @@
  * Used for strongtyping {@link Classes} decorator.
  * @internal
  */
-export type ClassesGetter<T> = {[K in keyof T]: boolean} & {[k: string]: boolean};
+export type ClassesGetter<T> = {[K in keyof T]: boolean | string} & {[k: string]: boolean | string};
