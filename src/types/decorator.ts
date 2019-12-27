ff4ca3c5c9406879277ab63c6e75d3be717ac1e2
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-12-27 03:35:25 +0100

    fix: return type of decorator

--- src/types/decorator.ts
+++ src/types/decorator.ts
@@ -6,4 +6,4 @@ export type TypedPropertyDecorator<T, C = any, K extends keyof C = keyof C> = (
   target: C,
   key: K,
   desc?: TypedPropertyDescriptor<T>
-) => TypedPropertyDescriptor<T> | void;
+) => any;
